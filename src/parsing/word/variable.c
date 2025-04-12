7828deae86e33f9c4aa5e7a1e6c4086c484af619
Your Name <a.mura27400@gmail.com>
2025-04-12 11:55:46 +0200

    fix(expanding): memory leaks

--- src/parsing/word/variable.c
+++ src/parsing/word/variable.c
@@ -5,12 +5,15 @@ static void	_override_var(char **word, t_var var)
 {
 	char	*var_name;
 	size_t	var_name_len;
+	char	*var_name_value;
 
 	var_name_len = (var.name_end - var.name_start) + 1;
 	var_name = mem_alloc(E_LFT_TASK, ((var_name_len) + 1) * sizeof(char));
 	var_name = str_memcpy(var_name, *word + var.name_start, var_name_len - 1);
 	var_name[var_name_len - 1] = '\0';
-	str_replace(word, env_get_var_value(var_name), var.start, var.end);
+	var_name_value = env_get_var_value(var_name);
+	mem_add_block(E_LFT_TASK, var_name_value);
+	str_replace(word, var_name_value, var.start, var.end);
 }
 
 static bool	_is_valid_var_name(char *word, size_t index, size_t var_name_start)
--- src/tools/string/str_replace.c
+++ src/tools/string/str_replace.c
@@ -1,4 +1,5 @@
 #include "str.h"
+#include "mem.h"
 
 void	str_replace(char **str, char *rep, int start, int end)
 {
@@ -12,12 +13,13 @@ void	str_replace(char **str, char *rep, int start, int end)
 	rep_l = str_len(rep);
 	str_l = str_len(old_str);
 	new_word_l = str_l - (end - start) + rep_l;
-	new_word = calloc((new_word_l + 1), sizeof(char));
+	new_word =	mem_alloc(E_LFT_TASK, (new_word_l + 1) * sizeof(char));
 	if (!new_word)
 		return;
 	str_memcpy(new_word, old_str, start);
 	str_memcpy(new_word + start, rep, rep_l);
 	str_memcpy(new_word + start + rep_l, old_str + end, str_l - end);
 	new_word[new_word_l] = '\0';
+	*str = NULL;
 	*str = new_word;
 }
