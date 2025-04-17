efe065f12835295b757f28fb3e86016ccef341bb
nevadeon <github@glhf.slmail.me>
2025-04-17 18:06:08 +0200

    feat(list): added lst_to_array and lst_len

---
+++ src/tools/list/lst_to_array.c
@@ -0,0 +1,18 @@
+#include "list.h"
+#include "mem.h"
+
+void	**lst_to_array(t_lifetime lft, t_list *list)
+{
+	void	**tab;
+	int		i;
+
+	tab = mem_alloc(lft, sizeof(char *) * (lst_len(list) + 1));
+	i = 0;
+	while (list)
+	{
+		tab[i++] = list->content;
+		list = list->next;
+	}
+	tab[i + 1] = NULL;
+	return (tab);
+}
