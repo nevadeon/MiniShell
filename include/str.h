b61f20344600209e30c17f77f23eb2e4e8b7e5dd
nevadeon <github@glhf.slmail.me>
2025-04-17 18:02:30 +0200

    feat(str): added str_split

--- include/str.h
+++ include/str.h
@@ -22,5 +22,6 @@ void	*str_memcpy(void *dst, void *src, size_t n);
 char	*str_vjoin(t_lifetime lft, size_t nb_args, ...);
 void	str_replace(t_lifetime lft, t_replace data);
 int		str_cmp(const char *s1, const char *s2);
+char	**str_split(t_lifetime lft, char const *s, char c);
 
 #endif
