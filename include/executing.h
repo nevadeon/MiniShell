703e7dec7bac9e42b0440dfb263b6c2b6b5e523c
nevadeon <github@glhf.slmail.me>
2025-04-22 19:27:06 +0200

    feat(exec): added exec_cmd

--- include/executing.h
+++ include/executing.h
@@ -4,7 +4,11 @@
 # include <unistd.h>
 # include <sys/wait.h>
 
+# define CMD_NAME_INDEX 0
 
+typedef struct s_str_list t_str_list;
+
+void	exec_cmd(char **env_paths, t_str_list *arg_list);
 void	dup_close(int source_fd, int dest_fd);
 
 #endif
