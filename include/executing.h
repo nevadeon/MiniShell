3ade68b560f379e38d14c49f7424f1e53af57437
nevadeon <github@glhf.slmail.me>
2025-04-23 18:25:01 +0200

    feat(exec): added lst_pid_new

--- include/executing.h
+++ include/executing.h
@@ -6,9 +6,24 @@
 
 # define CMD_NAME_INDEX 0
 
-typedef struct s_str_list t_str_list;
+typedef struct s_str_list	t_str_list;
+typedef struct s_ast		t_ast;
 
-void	exec_cmd(char **env_paths, t_str_list *arg_list);
-void	dup_close(int source_fd, int dest_fd);
+typedef enum e_error
+{
+	OK,
+	ERR_PIPE,
+	ERR_UNEXPECTED_NODE_TYPE,
+}	t_error;
+
+typedef struct s_pid_list
+{
+	struct s_pid_list	*next;
+	pid_t				pid;
+}	t_pid_list;
+
+void		exec_cmd(char **env_paths, t_str_list *arg_list);
+void		dup_close(int source_fd, int dest_fd);
+t_pid_list	*lst_pid_new(pid_t pid);
 
 #endif
