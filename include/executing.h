808043c28e223950f241679ab55d8553591ec415
nevadeon <github@glhf.slmail.me>
2025-04-23 18:26:07 +0200

    feat(exec): added execute_ast

--- include/executing.h
+++ include/executing.h
@@ -22,6 +22,17 @@ typedef struct s_pid_list
 	pid_t				pid;
 }	t_pid_list;
 
+typedef struct s_exec_data
+{
+	t_ast		*root;
+	t_ast		*last_ope;
+	int			prev_pipe_fd;
+	int			pipefd[2];
+	char		**env_paths;
+	t_pid_list	*pid_list;
+}	t_exec_data;
+
+void		execute_ast(t_ast *ast);
 void		exec_cmd(char **env_paths, t_str_list *arg_list);
 void		dup_close(int source_fd, int dest_fd);
 t_pid_list	*lst_pid_new(pid_t pid);
