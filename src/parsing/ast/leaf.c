270144c0f47cc410f829970cd2d42271936a7a31
Weldd2 <a.mura27400@gmail.com>
2025-05-08 13:32:12 +0000

    style(parsing): change file according to norm

--- src/parsing/ast/leaf.c
+++ src/parsing/ast/leaf.c
@@ -34,10 +34,44 @@ static t_redir_type	_is_redir(char *word)
 	return (E_REDIR_LAST_INDEX);
 }
 
+static void	_handle_redir(t_ast_data *data, char *word, t_redir_type type)
+{
+	t_redir_list	*redir_list;
+
+	redir_list = mem_alloc(E_LFT_TASK, sizeof(t_redir_list));
+	redir_list->content = get_next_word(&data->input);
+	redir_list->next = NULL;
+	redir_list->type = type;
+	if (type == E_REDIR_HEREDOC || type == E_REDIR_IN)
+		lst_add_back((t_list **)&data->prev->s_leaf.redir_in, (t_list *)redir_list);
+	else
+		lst_add_back((t_list **)&data->prev->s_leaf.redir_out, (t_list *)redir_list);
+}
+
+static void	_handle_prev_leaf(t_ast_data *data, char *word)
+{
+	t_strlist	*arg;
+
+	arg = mem_alloc(E_LFT_TASK, sizeof(t_strlist));
+	arg->content = word;
+	arg->next = NULL;
+	lst_add_back((t_list **)&data->prev->s_leaf.func, (t_list *)arg);
+}
+
+static void	_handle_leaf_parent(t_ast_data *data, t_ast *leaf)
+{
+	if (data->prev->type == E_NODE_OPE)
+	{
+		if (!data->prev->s_ope.left)
+			data->prev->s_ope.left = leaf;
+		else if (!data->prev->s_ope.right)
+			data->prev->s_ope.right = leaf;
+	}
+}
+
 void	handle_leaf(t_ast_data *data, char *word)
 {
 	t_redir_type	type;
-	t_redir_list	*redir_list;
 	t_ast			*leaf;
 
 	if (!data->prev)
@@ -47,36 +81,16 @@ void	handle_leaf(t_ast_data *data, char *word)
 	}
 	type = _is_redir(word);
 	if (type != E_REDIR_LAST_INDEX)
-	{
-		redir_list = mem_alloc(E_LFT_TASK, sizeof(t_redir_list));
-		redir_list->content = get_next_word(&data->input);
-		redir_list->next = NULL;
-		redir_list->type = type;
-		if (type == E_REDIR_HEREDOC || type == E_REDIR_IN)
-			lst_add_back((t_list **)&data->prev->s_leaf.redir_in, (t_list *)redir_list);
-		else
-			lst_add_back((t_list **)&data->prev->s_leaf.redir_out, (t_list *)redir_list);
-	}
+		_handle_redir(data, word, type);
 	else if (data->prev && data->prev->type == E_NODE_LEAF)
 	{
-		t_strlist	*arg;
-		arg = mem_alloc(E_LFT_TASK, sizeof(t_strlist));
-		arg->content = word;
-		arg->next = NULL;
-		if (data->prev->type == E_NODE_LEAF)
-			lst_add_back((t_list **)&data->prev->s_leaf.func, (t_list *)arg);
+		_handle_prev_leaf(data, word);
 		leaf = data->prev;
 	}
 	else
 	{
 		leaf = _create_leaf(word);
-		if (data->prev->type == E_NODE_OPE)
-		{
-			if (!data->prev->s_ope.left)
-				data->prev->s_ope.left = leaf;
-			else if (!data->prev->s_ope.right)
-				data->prev->s_ope.right = leaf;
-		}
+		_handle_leaf_parent(data, leaf);
 		data->prev = leaf;
 	}
 	if (!data->root)
