59f01db4e740914af8afb82ab6e9f4b56304fc12
Your Name <a.mura27400@gmail.com>
2025-05-11 19:29:58 +0200

    feat(tools): add error tools

---
+++ include/tools/error.h
@@ -0,0 +1,23 @@
+#ifndef ERROR_H
+# define ERROR_H
+
+# include "stdio.h"
+
+typedef struct s_error_category
+{
+	const char	*(*name)(void);
+	const char	*(*message)(int error);
+}	t_error_category;
+
+typedef struct s_error_code
+{
+	int						value;
+	const t_error_category	*error_category;
+}	t_error_code;
+
+t_error_code	make_error(int val, const t_error_category *cat);
+
+const char		*error_name(const t_error_code *ec);
+const char		*error_message(const t_error_code *ec);
+
+#endif
