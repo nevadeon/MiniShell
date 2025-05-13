3e111ad0fff57b7580f631c04346df69adb79c55
Your Name <a.mura27400@gmail.com>
2025-05-13 15:02:09 +0200

    refactor(wildcard): wildcard expanding is now well structured

---
+++ src/parsing/word/wildcard/compute.c
@@ -0,0 +1,53 @@
+#include "minishell.h"
+#include "path.h"
+
+static bool	_match(const char *pattern, const char *str)
+{
+	if (*pattern == '\0')
+		return (*str == '\0');
+	if (*pattern == '*')
+	{
+		while (*pattern == '*')
+			pattern++;
+		if (*pattern == '\0')
+			return (true);
+		while (*str)
+		{
+			if (_match(pattern, str))
+				return (true);
+			str++;
+		}
+		return (false);
+	}
+	else
+	{
+		if (*pattern == *str)
+			return (_match(pattern + 1, str + 1));
+		else
+			return (false);
+	}
+}
+
+t_strlist	*compute_pattern(t_strlist *files, char *pattern)
+{
+	char		*file_n;
+	t_strlist	*rep;
+	t_strlist	*current;
+
+	current = files;
+	rep = NULL;
+	while (current)
+	{
+		file_n = path_get_filename(current->content);
+		if (pattern[0] != '.' && file_n[0] == '.')
+		{
+			current = current->next;
+			continue ;
+		}
+		if (_match(pattern, file_n))
+			lst_add_back((t_list **)&rep,
+				lst_new(E_LFT_TASK, current->content));
+		current = current->next;
+	}
+	return (rep);
+}
