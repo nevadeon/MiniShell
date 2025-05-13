ec3074c81982ea01d8512f0b97031bc65fbed047
Your Name <a.mura27400@gmail.com>
2025-05-13 17:29:23 +0200

    refactor(path): improve path check function

--- include/tools/path.h
+++ include/tools/path.h
@@ -4,10 +4,22 @@
 # include <sys/stat.h>
 # include <dirent.h>
 # include <stdio.h>
+# include <unistd.h>
 # include "mem.h"
 # include "list.h"
 # include "str.h"
 
+typedef enum e_path_check
+{
+	E_PATH_OK = 0,
+	E_PATH_IS_FILE = 1 << 0,
+	E_PATH_EXIST = 1 << 1,
+	E_PATH_IS_DIRECTORY = 1 << 2,
+	E_PATH_RIGHT = 1 << 3,
+	E_PATH_LAST_INDEX
+}	t_path_check;
+
+bool	path_check(char *path, t_path_check check);
 t_list	*path_get_dir_content(char *path);
 char	*path_get_path(char *filepath);
 char	*path_get_filename(char *word);
