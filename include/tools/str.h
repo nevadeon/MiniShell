3e8c1a16c964008d1d567d0576b17937868e9157
Your Name <a.mura27400@gmail.com>
2025-05-13 14:59:07 +0200

    feat(tools): add str_dup

--- include/tools/str.h
+++ include/tools/str.h
@@ -19,6 +19,7 @@ int		str_ncmp(char *s1, char *s2, size_t n);
 bool	str_equals(const char *str1, const char *str2);
 size_t	str_len(const char *str);
 void	*str_memcpy(void *dst, void *src, size_t n);
+char	*str_dup(t_lifetime lft, char *s);
 char	*str_vjoin(t_lifetime lft, size_t nb_args, ...);
 void	str_replace(t_lifetime lft, t_replace data);
 int		str_cmp(const char *s1, const char *s2);
