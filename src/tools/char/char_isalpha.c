d77c913dfb812737488697485104df8db99b510a
Your Name <a.mura27400@gmail.com>
2025-05-14 17:36:17 +0200

    feat(tools): add char_isnum function

---
+++ src/tools/char/char_isalpha.c
@@ -0,0 +1,6 @@
+#include "char.h"
+
+bool	char_isalpha(char c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
---
+++ src/tools/char/char_isnum.c
@@ -0,0 +1,6 @@
+#include "char.h"
+
+bool	char_isnum(char c)
+{
+	return (c >= '0' && c <= '9');
+}
