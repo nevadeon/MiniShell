93afa3c9cf1086f2afe7767a6a1e27d7108ddf18
Weldd2 <a.mura27400@gmail.com>
2025-05-15 12:22:39 +0000

    feat(signals): add signals handling

---
+++ src/signals/signals.c
@@ -0,0 +1,17 @@
+#include "signals.h"
+#include "minishell.h"
+
+sig_atomic_t	g_signal = 0;
+
+void	signal_handler(int sig)
+{
+	g_signal = sig;
+
+	if (sig == SIGINT)
+	{
+		write(STDIN_FILENO, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
