1edb99bd7abd286185b4290760120388ce37b747
nevadeon <github@glhf.slmail.me>
2025-05-29 18:25:05 +0200

    feat: added malloc_allocator

--- src/tools/memory/allocator.c
+++ src/tools/memory/allocator.c
@@ -1,29 +1,23 @@
 #include "allocator.h"
 
-static void	assert_allocator(t_allocator *allocator)
-{
-	assert(allocator);
-	assert(allocator->data);
-	assert(allocator->alloc_fn);
-	assert(allocator->check_fn);
-	assert(allocator->free_fn);
-}
-
 void	*mem_alloc(t_allocator *allocator, size_t size)
 {
-	assert_allocator(allocator);
+	assert(allocator);
+	assert(allocator->alloc_fn);
 	return (allocator->alloc_fn(allocator->data, size));
 }
 
 bool	check_allocator(t_allocator *allocator)
 {
-	assert_allocator(allocator);
+	assert(allocator);
+	assert(allocator->check_fn);
 	return (allocator->check_fn(allocator->data));
 }
 
 void	free_allocator(t_allocator *allocator)
 {
-	assert_allocator(allocator);
+	assert(allocator);
+	assert(allocator->free_fn);
 	allocator->free_fn(allocator->data);
 	allocator->data = NULL;
 	allocator->alloc_fn = NULL;
