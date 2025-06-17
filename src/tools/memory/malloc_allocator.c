db33b40b6f9b018e9c4cf5f7910b944cc2155041
nevadeon <github@glhf.slmail.me>
2025-06-17 14:54:28 +0200

    fix: changed malloc_allocator prototype and added free_fn

--- src/tools/memory/malloc_allocator.c
+++ src/tools/memory/malloc_allocator.c
@@ -7,17 +7,24 @@ void	*malloc_alloc_fn(void *data, size_t size)
 	return (malloc(size));
 }
 
-t_alloc	*new_malloc_allocator(void)
+t_alloc	*new_malloc_allocator(int unused_param)
 {
 	t_alloc	*malloc_allocator;
 
+	(void)unused_param;
 	malloc_allocator = malloc(sizeof(t_alloc));
 	if (!malloc_allocator)
 		return (NULL);
 	*malloc_allocator = (t_alloc){
 		.data = NULL,
 		.alloc_fn = malloc_alloc_fn,
-		.free_fn = NULL,
+		.free_fn = malloc_free_fn,
 	};
 	return (malloc_allocator);
+}
+
+void	malloc_free_fn(void *data)
+{
+	(void)data;
+	return ;
 }
