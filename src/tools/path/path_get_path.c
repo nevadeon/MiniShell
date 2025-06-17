b12fcc864731b2408c106ad3d7bb8e60f5eae2b5
Weldd2 <a.mura27400@gmail.com>
2025-06-17 12:24:36 +0000

    fix: unused values

--- src/tools/path/path_get_path.c
+++ src/tools/path/path_get_path.c
@@ -6,9 +6,13 @@ char	*path_get_path(t_alloc *alloc, char *filepath)
 	size_t	last_slash_index;
 	char	*path_cpy;
 
+	if (!filepath)
+		return (str_dup(alloc, ""));
 	last_slash_index = 0;
 	index = 0;
 	path_cpy = str_dup(alloc, filepath);
+	if (!path_cpy)
+		return (NULL);
 	while (path_cpy[index])
 	{
 		if (path_cpy[index] == '/')
