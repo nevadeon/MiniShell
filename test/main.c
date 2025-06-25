c83798dbdf13a0866ee3a4ecb33dc3234bdbbfe9
Weldd2 <a.mura27400@gmail.com>
2025-06-25 15:39:52 +0200

    fix: update tests

--- test/main.c
+++ test/main.c
@@ -3,6 +3,6 @@
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char ** argv, char **envp)
 {
 	test_memory();
-	test_parsing();
+	test_parsing(&envp);
 	return (EXIT_SUCCESS);
 }
