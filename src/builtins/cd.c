80a1ea156b7671883e0bc40e18bf07225ef0b5f9
Weldd2 <a.mura27400@gmail.com>
2025-06-26 16:27:11 +0200

    fix: cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,36 +3,36 @@
 #include "tools/env.h"
 #include "cbuiltins.h"
 
-// static const char	*g_cd_error_msg[] = {
-// 	E_ERR_CD_OK = "No error",
-// 	E_ERR_CD_ARGS = "too many arguments",
-// 	E_ERR_CD_FILE_EXIST = "No such file or directory",
-// 	E_ERR_CD_NOT_DIR = "Not a directory"
-// }
+static int	_print_error(t_ctx *ctx, const char *msg, const char *arg)
+{
+	if (arg)
+		fprintf(stderr, "bash: cd: %s: %s\n", arg, msg);
+	else
+		fprintf(stderr, "bash: cd: %s\n", msg);
+	ctx->last_exit_code = 1;
+	return (1);
+}
 
-// static void	_print_err(t_error_cd e, char *path)
-// {
-// 	printf("[cd] %s", g_cd_error_msg[e]);
-// 	if (path)
-// 		printf(": %s", path);
-// 	printf("\n");
-// }
+static char	*get_path_value(t_ctx *ctx, char **args)
+{
+	char	*oldpwd;
+	char	*home;
 
-void print_error(const char *first, ...) {
-	va_list	args;
-	char	*str;
-
-	va_start(args, first);
-	printf("bash: %s", first);
-	while (1)
+	if (args[1] && args[2])
+		return (_print_error(ctx, "too many arguments", NULL), NULL);
+	if (args[1] && str_equals(args[1], "-"))
 	{
-		str = va_arg(args, char *);
-		if (!str)
-			break ;
-		printf(": %s", str);
+		oldpwd = env_get_var_value(*ctx->env, "OLDPWD");
+		if (!oldpwd)
+			return (_print_error(ctx, "OLDPWD not set", NULL), NULL);
+		return (oldpwd);
 	}
-	printf("\n");
-	va_end(args);
+	if (args[1])
+		return (args[1]);
+	home = env_get_var_value(*ctx->env, "HOME");
+	if (!home)
+		return (_print_error(ctx, "HOME not set", NULL), NULL);
+	return (home);
 }
 
 int	builtin_cd(t_ctx *ctx, char **args)
@@ -40,24 +40,17 @@ int	builtin_cd(t_ctx *ctx, char **args)
 	char	cwd[PATH_MAX];
 	char	*path;
 
-	if (args[1] && args[2])
-		return (print_error("cd: too many arguments", NULL), 1);
-	if (args[1])
-	{
-		path = args[1];
-		if (str_equals(path, "-"))
-			path = env_get_var_value(*ctx->env, "OLDPWD");
-	}
-	else
-	{
-		path = env_get_var_value(*ctx->env, "HOME");
-		if (!path)
-			return (print_error("cd: HOME not set", NULL), 1);
-	}
+	path = get_path_value(ctx, args);
+	if (!path)
+		return (1);
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		cwd[0] = '\0';
 	if (chdir(path))
-		return (print_error("cd", path, strerror(errno), NULL), 1);
+		return (_print_error(ctx, "no such file or directory", path));
 	env_set_var_value(ctx, str_vjoin(*ctx->prog, 2, "OLDPWD=", cwd));
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		cwd[0] = '\0';
+	env_set_var_value(ctx, str_vjoin(*ctx->prog, 2, "PWD=", cwd));
+	ctx->last_exit_code = 0;
 	return (0);
 }
