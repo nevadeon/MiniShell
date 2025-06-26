867e99b5b435bfd4caafb2d6dce1b4cee03f4ebd
nevadeon <github@glhf.slmail.me>
2025-06-26 21:03:10 +0200

    fix: pls stop breaking echo I beg

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -10,7 +10,7 @@ int	builtin_echo(t_ctx *unused_ctx, char **args)
 	if (!args[1])
 		return (printf("\n"), 0);
 	newline = (str_cmp(args[1], "-n") != 0);
-	i = newline;
+	i = !newline + 1;
 	printf("%s", args[i]);
 	while (args[++i])
 		printf(" %s", args[i]);
