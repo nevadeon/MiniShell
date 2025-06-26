71ce00d0d42d6c9221f05135dc9c26b4952f135a
Weldd2 <a.mura27400@gmail.com>
2025-06-26 16:44:45 +0200

    fix: exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,21 +1,31 @@
 #include "cbuiltins.h"
 
-int	builtin_exit(t_ctx *unused_ctx, char **args)
+static int	_print_error(t_ctx *ctx, const char *msg, const char *arg)
+{
+	if (arg)
+		fprintf(stderr, "bash: exit: %s: %s\n", arg, msg);
+	else
+		fprintf(stderr, "bash: exit: %s\n", msg);
+	ctx->last_exit_code = 1;
+	return (1);
+}
+
+int	builtin_exit(t_ctx *ctx, char **args)
 {
 	int	status;
 	int	i;
 
-	(void)unused_ctx;
+	printf("exit\n");
 	if (args[1] && args[2])
-		return (printf("bash: exit: too many arguments"), 1);
+		return (_print_error(ctx, "too many arguments", NULL));
 	if (!args[1])
 		exit(EXIT_SUCCESS);
 	i = -1;
-	while (args[1][i])
+	while (args[1][++i])
 	{
 		if (!char_isnum(args[1][i]))
 		{
-			printf("bash: exit: %s: numeric argument required", args[1]);
+			_print_error(ctx, "numeric argument required", args[1]);
 			exit(2);
 		}
 	}
