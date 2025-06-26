e50c3c02d893fc8889bc381df8a5e21871b77d92
Weldd2 <a.mura27400@gmail.com>
2025-06-26 19:07:27 +0000

    style: add newline to pwd error

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -7,7 +7,7 @@ int	builtin_pwd(t_ctx *ctx, char **unused_args)
 	(void)unused_args;
 	pwd = env_get_var_value(*ctx->env, "PWD");
 	if (!pwd)
-		return (printf("bash: pwd: PWD not set"), 1);
+		return (printf("bash: pwd: PWD not set\n"), 1);
 	printf("%s\n", pwd);
 	return (0);
 }
