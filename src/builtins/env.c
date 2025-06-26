212edd78eff5aa0833aae164218c22c800b85250
Weldd2 <a.mura27400@gmail.com>
2025-06-26 16:27:29 +0200

    feat: add errors to env builtin

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,11 +1,20 @@
 #include "cbuiltins.h"
 #include "allocators/allocator.h"
 
+static int	_print_error(t_ctx *ctx, const char *msg)
+{
+	fprintf(stderr, "bash: env: %s\n", msg);
+	ctx->last_exit_code = 1;
+	return (1);
+}
+
 int	builtin_env(t_ctx *ctx, char **unused_args)
 {
 	char	**env;
 	int		i;
 
+	if (unused_args[1])
+		return (_print_error(ctx, "too many arguments"));
 	(void)unused_args;
 	env = *ctx->env;
 	i = -1;
