7b145120d658a3f7a8c73fcc9d55be695164a61f
Weldd2 <a.mura27400@gmail.com>
2025-06-26 17:04:43 +0200

    style: norm

--- include/tools/sort.h
+++ include/tools/sort.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 16:01:51 by nevadeon          #+#    #+#             */
+/*   Updated: 2025/06/26 17:04:14 by antoinemura      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SORT_H
 # define SORT_H
 
@@ -5,6 +17,16 @@
 # include "tools/list.h"
 
 typedef struct s_list	t_list;
+
+typedef struct s_partition
+{
+	t_list	*list;
+	t_list	*pivot;
+	int		(*compare)(void *, void *);
+	t_list	**smaller;
+	t_list	**equal;
+	t_list	**greater;
+}	t_partition;
 
 t_list	*ft_qsort(t_list *list, int (*f)(void *, void *));
 int		ascii_sort(void *a, void *b);
