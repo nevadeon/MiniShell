966912593434875ae67c775a3ebc2488b9daed35
nevadeon <github@glhf.slmail.me>
2025-06-28 19:16:03 +0200

    fix: echo n option behaves like bash now

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,19 +1,42 @@
 #include "minishell.h"
 #include "cbuiltins.h"
 
+static bool	_is_option(const char *str, char c)
+{
+	int	i;
+
+	if (str[0] != '-')
+		return (false);
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] != c)
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	builtin_echo(t_ctx *unused_ctx, char **args)
 {
 	bool	newline;
 	int		i;
 
 	(void)unused_ctx;
-	if (!args[1])
-		return (printf("\n"), 0);
-	newline = (str_cmp(args[1], "-n") != 0);
-	i = !newline + 1;
-	printf("%s", args[i]);
-	while (args[++i])
-		printf(" %s", args[i]);
+	newline = true;
+	i = 1;
+	while (args[i] && _is_option(args[i], 'n'))
+	{
+		newline = false;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
 	if (newline)
 		printf("\n");
 	return (0);
