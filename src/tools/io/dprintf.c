bd721d3c203059a3caef9e9d6c18c2a157e62595
Weldd2 <a.mura27400@gmail.com>
2025-06-29 21:31:47 +0200

    fix: io_dprintf

--- src/tools/io/dprintf.c
+++ src/tools/io/dprintf.c
@@ -1,21 +1,21 @@
 #include "tools/io.h"
 
-static size_t	_dprintarg(int fd, char c, va_list args)
+static size_t	_dprintarg(int fd, char c, va_list *args)
 {
 	if (c == 'c')
-		return (io_dputchar(fd, (unsigned char) va_arg(args, int)));
+		return (io_dputchar(fd, (unsigned char) va_arg(*args, int)));
 	else if (c == 's')
-		return (io_dputstr(fd, va_arg(args, char *)));
+		return (io_dputstr(fd, va_arg(*args, char *)));
 	else if (c == 'p')
-		return (io_dputptr(fd, va_arg(args, void *)));
+		return (io_dputptr(fd, va_arg(*args, void *)));
 	else if (c == 'd' || c == 'i')
-		return (io_dputlbase(fd, (long) va_arg(args, int), DEC));
+		return (io_dputlbase(fd, (long) va_arg(*args, int), DEC));
 	else if (c == 'u')
-		return (io_dputulbase(fd, (uint64_t) va_arg(args, uint32_t), DEC));
+		return (io_dputulbase(fd, (uint64_t) va_arg(*args, uint32_t), DEC));
 	else if (c == 'x')
-		return (io_dputulbase(fd, (uint64_t) va_arg(args, uint32_t), HEXA_LOW));
+		return (io_dputulbase(fd, (uint64_t) va_arg(*args, uint32_t), HEXA_LOW));
 	else if (c == 'X')
-		return (io_dputulbase(fd, (uint64_t) va_arg(args, uint32_t), HEXA_UPP));
+		return (io_dputulbase(fd, (uint64_t) va_arg(*args, uint32_t), HEXA_UPP));
 	else if (c == '%')
 		return (io_dputchar(fd, '%'));
 	return (0);
@@ -31,7 +31,7 @@ size_t	io_dprintf(int fd, const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%')
-			nb_printed += _dprintarg(fd, *(++format), args);
+			nb_printed += _dprintarg(fd, *(++format), &args);
 		else
 			nb_printed += io_dputchar(fd, *format);
 		format++;
