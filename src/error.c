0d5fd2af278111f5c6bd3b8830cd88a0d12fee9c
Weldd2 <a.mura27400@gmail.com>
2025-06-29 15:29:19 +0200

    refactor: add error enum

--- src/error.c
+++ src/error.c
@@ -1,25 +1,25 @@
 #include "minishell.h"
 
 static const char	*_err_msg[] = {
-	"",
-	"bad substitution",
-	"syntax error near expected token `",
-	"unexpected EOF while looking for matching `",
-	"Permission denied",
-	"No such file or directory",
-	"Is a directory",
-	"command not found",
+	[ERR_NONE] = "",
+	[ERR_BAD_SUBSTITUTION] = "bad substitution",
+	[ERR_UNEXPECTED_TOKEN] = "syntax error near expected token `",
+	[ERR_UNCLOSED] = "unexpected EOF while looking for matching `",
+	[ERR_PERM_DENIED] = "Permission denied",
+	[ERR_NO_FILE_OR_DIR] = "No such file or directory",
+	[ERR_IS_DIR] = "Is a directory",
+	[ERR_CMD_NOT_FOUND] = "command not found",
 };
 
 static const int	_err_code[] = {
-	0,
-	1,
-	2,
-	2,
-	126,
-	127,
-	127,
-	127
+	[ERR_NONE] = 0,
+	[ERR_BAD_SUBSTITUTION] = 1,
+	[ERR_UNEXPECTED_TOKEN] = 2,
+	[ERR_UNCLOSED] = 2,
+	[ERR_PERM_DENIED] = 126,
+	[ERR_NO_FILE_OR_DIR] = 127,
+	[ERR_IS_DIR] = 127,
+	[ERR_CMD_NOT_FOUND] = 127
 };
 
 static const size_t	_err_msg_size = \
