96f6478db1f8c27dac04bb9e4e7d473acd92755b
Weldd2 <a.mura27400@gmail.com>
2025-06-29 17:15:41 +0200

    fix: tokenize

--- src/parsing/tokenize.c
+++ src/parsing/tokenize.c
@@ -5,14 +5,32 @@ static char	*tokenize_meta(t_ctx *ctx, char **input)
 	if (**input == '|')
 		return (str_dup(*ctx->cmd, "|"));
 	else if (**input == '<' && *(*input + 1) == '<')
-		return (str_dup(*ctx->cmd, "<<"));
+		return (*input += 2, str_dup(*ctx->cmd, "<<"));
 	else if (**input == '>' && *(*input + 1) == '>')
-		return (str_dup(*ctx->cmd, ">>"));
+		return (*input += 2, str_dup(*ctx->cmd, ">>"));
 	else if (**input == '<')
-		return (str_dup(*ctx->cmd, "<"));
+		return ((*input)++, str_dup(*ctx->cmd, "<"));
 	else if (**input == '>')
-		return (str_dup(*ctx->cmd, ">"));
+		return ((*input)++, str_dup(*ctx->cmd, ">"));
+	else if (**input == '|')
+		return ((*input)++, str_dup(*ctx->cmd, "|"));
 	return (NULL);
+}
+
+static bool	handle_quote_char(char **input, size_t *index, t_ctx *ctx)
+{
+	char	quote;
+
+	quote = (*input)[*index];
+	if (quote == '"')
+		return (str_escape(*input, index, '"', '"')
+			|| (throw_error(ctx, E_UNCLOSED, "\""), false));
+	if (quote == '\'')
+		return (str_escape(*input, index, '\'', '\'')
+			|| (throw_error(ctx, E_UNCLOSED, "'"), false));
+	if (quote == '{')
+		return (str_escape(*input, index, '{', '}'), true);
+	return (false);
 }
 
 static char	*_process_tokenize(t_ctx *ctx, char **input)
@@ -20,26 +38,25 @@ static char	*_process_tokenize(t_ctx *ctx, char **input)
 	size_t	index;
 	char	*ret;
 
-	while (*input && is_blank_meta(**input))
+	while (**input && is_blank_meta(**input))
 		(*input)++;
-	if (!*input || str_len(*input) == 0)
+	if (!**input)
 		return (NULL);
 	ret = tokenize_meta(ctx, input);
 	if (ret)
-		return (*input += str_len(ret), ret);
+		return (ret);
 	index = 0;
 	while ((*input)[index] && !is_meta((*input)[index]))
 	{
-		if (!str_escape(*input, &index, '"', '"'))
-			return (throw_error(ctx, E_UNCLOSED, "\""), NULL);
-		if (!str_escape(*input, &index, '\'', '\''))
-			return (throw_error(ctx, E_UNCLOSED, "'"), NULL);
-		str_escape(*input, &index, '{', '}');
-		if ((*input)[index] && !is_meta((*input)[index]))
+		if (handle_quote_char(input, &index, ctx))
+			continue ;
+		if (!ctx->last_error_type)
 			index++;
+		else
+			return (NULL);
 	}
 	ret = str_extract(*ctx->cmd, *input, 0, index);
-	*input += str_len(ret);
+	*input += index;
 	return (ret);
 }
 
@@ -56,6 +73,8 @@ t_token_list	*tokenize(t_ctx *ctx, char **input)
 		if (ctx->last_error_type)
 			return (NULL);
 		tok = new_token(*ctx->cmd, word, false, get_token_type(word));
+		if (!tok)
+			return (NULL);
 		lst_add_back((t_list **)&token_list, lst_new(*ctx->cmd, (void *)tok));
 		word = _process_tokenize(ctx, input);
 	}
