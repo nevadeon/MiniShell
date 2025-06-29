0767ebb308a5eb7fa7fcda217cc5e58537712e97
Weldd2 <a.mura27400@gmail.com>
2025-06-29 17:15:21 +0200

    fix: str_escape

--- src/tools/string/str_escape.c
+++ src/tools/string/str_escape.c
@@ -3,13 +3,17 @@
 bool	str_escape(char *s, size_t *index, char start, char end)
 {
 	assert(s);
-	if (!s[*index])
-		return (true);
-	if (s[*index] != start)
+	assert(index);
+
+	if (!s[*index] || s[*index] != start)
 		return (true);
 	(*index)++;
 	while (s[*index] && s[*index] != end)
+	{
+		if (s[*index] == '\\' && s[*index + 1])
+			(*index)++;
 		(*index)++;
+	}
 	if (s[*index] == end)
 	{
 		(*index)++;
