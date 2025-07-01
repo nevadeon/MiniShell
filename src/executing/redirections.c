0c9e3f93e42075f5ccb6ec681065d35bf2bab29a
nevadeon <github@glhf.slmail.me>
2025-07-01 22:14:39 +0200

    fix: fd closing in exec

--- src/executing/redirections.c
+++ src/executing/redirections.c
@@ -1,26 +1,37 @@
 #include "executing.h"
 
-void	handle_redirections(int redir[2], t_fds pipe)
+void	handle_redirections(int redir[2], int pipe_out, int pipe_in, int to_close)
 {
-	if (redir[IN] > 0)
+	assert(redir[IN] > 2 || redir[IN] == NO_REDIR);
+	assert(redir[OUT] > 2 || redir[OUT] == NO_REDIR);
+	assert(pipe_in > 2 || pipe_in == NO_REDIR);
+	assert(pipe_out > 2 || pipe_out == NO_REDIR);
+	assert(to_close > 2 || to_close == NO_REDIR);
+	if (redir[IN] >= 0)
 		dup2(redir[IN], STDIN_FILENO);
-	else
-		dup2_close(pipe.in, STDIN_FILENO);
-	if (redir[OUT] > 0)
-		dup2(redir[OUT], STDIN_FILENO);
-	else
-		dup2_close(pipe.out, STDIN_FILENO);
-	if (pipe.to_close > 0)
-		close(pipe.to_close);
+	else if (pipe_out >= 0)
+		dup2(pipe_out, STDIN_FILENO);
+	if (redir[OUT] >= 0)
+		dup2(redir[OUT], STDOUT_FILENO);
+	else if (pipe_in >= 0)
+		dup2(pipe_in, STDOUT_FILENO);
+	if (pipe_in >= 0)
+		close(pipe_in);
+	if (pipe_out >= 0)
+		close(pipe_out);
+	if (to_close >= 0)
+		close(to_close);
 }
 
 void	close_redirections(t_ast *ast)
 {
 	if (ast->type == NODE_LEAF)
 	{
-		if (ast->leaf.redir[IN] > 0)
+		assert(ast->leaf.redir[IN] > 2 || ast->leaf.redir[IN] == NO_REDIR);
+		assert(ast->leaf.redir[OUT] > 2 || ast->leaf.redir[OUT] == NO_REDIR);
+		if (ast->leaf.redir[IN] > STDERR)
 			close(ast->leaf.redir[IN]);
-		if (ast->leaf.redir[OUT] > 0)
+		if (ast->leaf.redir[OUT] > STDERR)
 			close(ast->leaf.redir[OUT]);
 	}
 	else
